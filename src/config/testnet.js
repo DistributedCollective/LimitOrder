131e919ad68efbac704d3bd656dd45928b1b9ec7
Ubuntu <ubuntu@ip-172-31-4-203.us-west-2.compute.internal>
2022-02-04 11:56:25 +0000

    Refactor testscripts

--- src/config/testnet.js
+++ src/config/testnet.js
@@ -2,13 +2,13 @@ module.exports = {
     port: 3001,
     chainId: 31,
     orderBookChainId: 31,
-    baseAPIUrl: 'http://18.217.222.156:3001',
+    baseAPIUrl: 'http://localhost:3001',
     networkUrl: 'https://testnet.sovryn.app/rpc',
     orderBookNetwork: 'https://testnet.sovryn.app/rpc',
     contracts: {
-        settlement: '0xb80B8AbF3674F7ace310d1EFF9546abAC7Cee9a4',
-        orderBook: '0x3aEAABbcc0b15d8Ad85dD6941AC86Fa8C97A00fa',
-        orderBookMargin: '0x8c10d2fB5BB144203751B496d6f7e746377cb50E',
+        settlement: '0x84F2456B16ba982A47cb623Fe7F4DaDaB896a0F5',
+        orderBook: '0xC5227dBc8EA9E22439f328A80b605B0cDfaD0C54',
+        orderBookMargin: '0xE222Ecb042565Ee577c34EC28686FFFd15675592',
         swap: '0x61172b53423e205a399640e5283e51fe60ec2256'
     },
     loanContracts: {
